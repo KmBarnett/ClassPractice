221b5998c2ef9b4c12fa0906eb1d5e5a0caa18eb
kmbarnett <kmbarnett94@gmail.com>
2019-12-10 08:23:20 -0700

    Add Vampire is done

--- exercises/vampire.js
+++ exercises/vampire.js
@@ -1,6 +1,12 @@
-class Vampire = {
-  constructor(){
-    
+class Vampire {
+  constructor(name, pet){
+    this.name = name
+    this.pet = pet || 'bat'
+    this.thirsty = true
+  }
+
+  drink(){
+    this.thirsty = false
   }
 
 }
